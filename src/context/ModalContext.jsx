9d3f0a50212989937f1e982c272136eb07cab3b8
David <davidramirez9210@gmail.com>
2021-05-07 16:50:39 -0300

    call to recipie API

--- src/context/ModalContext.jsx
+++ src/context/ModalContext.jsx
@@ -7,6 +7,20 @@ export const ModalContext = createContext();
 const ModalProvider = (props) => {
     //state del provider
     const [idreceta,guardarIdReceta] = useState(null);
+    const[receta,guardarReceta] = useState({});
+
+    //llamar api con la receta
+    useEffect(() => {
+        const obtenerReceta = async() =>{
+            if(!idreceta) return;
+
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+            const resultado = await axios.get(url);
+            guardarReceta(resultado.data.drinks[0]);
+
+        }
+        obtenerReceta();
+    },[idreceta]);
 
 
     return ( 
