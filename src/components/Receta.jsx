db792e3fe2ec6c287c1a37e31d63fc226cf75875
David <davidramirez9210@gmail.com>
2021-05-07 18:47:29 -0300

    finissh Receta Component

--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -17,7 +17,7 @@ function getModalStyle() {
 const useStyles = makeStyles(theme => ({
     paper: {
       position: 'absolute',
-      width: 600,
+      width: 450,
       backgroundColor: theme.palette.background.paper,
       boxShadow: theme.shadows[5],
       padding: theme.spacing(2, 4, 3),
@@ -42,8 +42,17 @@ const Receta = ({receta}) => {
     const {informacion,guardarIdReceta,guardarReceta} = useContext(ModalContext);
 
     //Muestra y formatea los ingredientes
-
-    mostrarIngredientes
+    const mostrarIngredientes = informacion => {
+        let ingredientes = [];
+        for(let i =1; i<16; i++) {
+            if( informacion[`strIngredient${i}`] ){
+                ingredientes.push(
+                    <li key={i}>{informacion[`strIngredient${i}`] } {informacion[`strMeasure${i}`] }</li>
+                )
+            }
+        }
+        return ingredientes;
+    }
     
     return ( 
         <div className="col-md-4 mb-3">
